caad130718baebeb823f8b04b10fcfb792cb81a7
squcir <1754956816@qq.com>
2020-06-10 17:59:54 +0800

    20200610

---
+++ 9-回文数/IsPalindrome.php
@@ -0,0 +1,50 @@
+<?php
+
+require '../Solution.php';
+
+/**
+ * @link https://leetcode-cn.com/problems/palindrome-number/
+ * @me yes
+ */
+class IsPalindrome extends Solution
+{
+
+    /**
+     * @param integer $x
+     * @return boolean
+     */
+    public function invoke($x)
+    {
+        //转string
+//        $x = (string) $x;
+//        $c = strlen($x);
+//
+//        for ($i=0,$j=$c-1; $i<=$j; $i++,$j--) {
+//            if ($x[$i]!=$x[$j]) {
+//                return false;
+//            }
+//        }
+//        return true;
+
+        //反转所有数字(可能会超过int最大值，不对)
+        if ($x < 0) {
+            return false;
+        }
+
+        $ix = $x; $t = 0;
+        do {
+            $a = $x % 10;
+            $x = ($x - $a) / 10;
+            $t = $t * 10 + $a;
+        } while($x != 0);
+
+        return $t == $ix;
+
+        // 反转一半
+    }
+
+}
+
+var_dump((new IsPalindrome())->invoke(121));
+var_dump((new IsPalindrome())->invoke(-121));
+var_dump((new IsPalindrome())->invoke(10));
